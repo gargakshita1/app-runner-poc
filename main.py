ade040e04df0f9456f57f35a161cfff2a2968592
gargakshita1 <akshitagarg@aimxd.ai>
2026-01-20 18:38:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {
-        "status": "ok",
-        "message": "FastAPI running on AWS App Runner (no Docker) 🚀"
-    }
-
-@app.get("/ping")
-def ping():
-    return "pong"
+    return {"message": "App Runner is working"}
