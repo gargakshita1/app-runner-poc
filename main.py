4fe9bd57eb146f15e6d1063eff0d15602112f920
gargakshita1 <akshitagarg@aimxd.ai>
2026-01-20 18:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "App Runner is working"}
+    return {"message": "App Runner is working 🚀"}
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8080,
+        log_level="info"
+    )
