fda3131bfaf960754fee2bb99cf44a2ef237d43b
gargakshita1 <akshitagarg@aimxd.ai>
2026-01-22 01:32:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ def ground(groups=get_groups):
 def satellite():
     return {"message": "Satellite access success"}
     
-if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8080,
-        log_level="info"
-    )
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=8080,
+#         log_level="info"
+#     )
     
